862e60f158b62cdaa19ef585fa99402d411bbbb6
Dr. Pedro Pinto <pedropinto.psicop@gmail.com>
2025-03-12 05:30:42 +0000

    hum

--- utils/goals_utils.py
+++ utils/goals_utils.py
@@ -450,7 +450,7 @@ def render_patient_goals(user_id):
         if metas:
             st.markdown(
                 f"""
-                <h4 style='color: #7159c1; font-size: 24px; font-weight: bold; margin-top: 20px;'>
+                <h4 style='color: #836fff; font-size: 24px; font-weight: bold; margin-top: 20px;'>
                 {prazo_labels[prazo]}
                 </h4>
                 """,
