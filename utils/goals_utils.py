9996d833434d1f7699e430a9944d9b5bbe78e44e
Dr. Pedro Pinto <pedropinto.psicop@gmail.com>
2025-03-12 02:41:37 +0000

    timedelta

--- utils/goals_utils.py
+++ utils/goals_utils.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from datetime import date
+from datetime import datetime, timedelta
 from auth import supabase_client
 from utils.user_utils import get_user_info
 from utils.date_utils import format_date
