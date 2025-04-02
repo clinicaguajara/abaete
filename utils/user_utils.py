0824ce96c4ccf0806436297d0d94dbbc2303c481
Clínica Guajará <pedropinto.psicop@gmail.com>
2025-04-02 01:49:52 +0000

    del

--- utils/user_utils.py
+++ utils/user_utils.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from auth import supabase_client
-from utils.professional_utils import is_professional_enabled
-from utils.profile_utils import render_onboarding_questionnaire
 
 
 # 💾 Função para cachear o perfil do usuário e evitar buscas repetitivas.
